c46f942d7ac654b4995920298a229100c6c3cddf
mardi2020 <mardi@khu.ac.kr>
2022-08-01 19:53:58 +0900

    util 패키지 이름 수정

--- src/main/java/com/app/vple/config/SecurityConfig.java
+++ src/main/java/com/app/vple/config/SecurityConfig.java
@@ -1,16 +1,13 @@
 package com.app.vple.config;
 
-import com.app.vple.Util.OAuth2AuthenticationSuccessHandler;
+import com.app.vple.util.OAuth2AuthenticationSuccessHandler;
 import com.app.vple.service.UserOAuth2Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
