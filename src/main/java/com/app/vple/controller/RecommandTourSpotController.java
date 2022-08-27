4c4418bedff006688b2bc6f5fdf4552a32734564
wjdrkdud5 <bangz_@naver.com>
2022-08-27 17:57:47 +0900

    recommand added

---
+++ src/main/java/com/app/vple/controller/RecommandTourSpotController.java
@@ -0,0 +1,32 @@
+package com.app.vple.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.vple.domain.dto.RecommandTourSpotDetailDto;
+import com.app.vple.service.RecommandTourSpotService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/recommand/tourspot")
+public class RecommandTourSpotController {
+
+    private final RecommandTourSpotService recommandTourSpotService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> recommandRestaurantDetails(@PathVariable Long id) {
+        try {
+            RecommandTourSpotDetailDto recommandTourSpot = recommandTourSpotService.findRecommandTourSpotDetails(id);
+            return new ResponseEntity<>(recommandTourSpot, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
